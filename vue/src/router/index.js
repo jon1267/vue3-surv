186d609a55ca9f75bd55fbeb931289e855229cc6
jon1267 <jonleev@gmail.com>
2022-07-12 17:04:26 +0300

    some routes tuning

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,12 +2,21 @@ import {createRouter, createWebHistory} from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import Surveys from '../views/Surveys.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import store from "../store";
 
 const routes = [
   {
     path: '/',
+    redirect: '/dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: DefaultLayout,
+    meta: { requiresAuth: true },
+    children: [
+      { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+      { path: '/surveys', name: 'Surveys', component: Surveys }
+    ]
   },
   {
     path: '/login',
@@ -24,6 +33,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({name: 'Login'});
+  } else {
+    next();
+  }
 });
 
 export default router;
